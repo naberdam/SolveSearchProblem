bec6bf2aa01a47297e5864095863f86da1c48904
Amitai-p <amitai.p2@gmail.com>
2020-01-12 18:25:26 +0200

    erase FilecacheManager

--- FileCacheManager.h
+++ FileCacheManager.h
@@ -4,6 +4,7 @@
 
 #ifndef SOLIDPROJECT_EX2_FILECACHEMANAGER_H
 #define SOLIDPROJECT_EX2_FILECACHEMANAGER_H
+
 #include <vector>
 #include <string>
 #include <unordered_map>
@@ -11,71 +12,17 @@
 #include <mutex>
 #include <sstream>
 #include "CacheManager.h"
+#include <iostream>
+#include <string.h>
+#include <list>
 
+#define CACHE_NAME_FILE "file"
+using namespace std;
 
-template <class Problem ,class Solution>
-class FileCacheManager: public CacheManager<Problem,Solution> {
+template<class Problem, class Solution>
+class FileCacheManager : public CacheManager<Problem, Solution> {
 public:
-    FileCacheManager();
-
-    ~FileCacheManager();
-
-    void load();
-
-    void save();
 };
 
-template<class Problem, class Solution>
-void FileCacheManager<Problem, Solution>::load() {
-    string line;
-    ifstream file;
-    file.open(CACHE_FILE_NAME, ios::in);
 
-    if (file.is_open()) {
-        while (getline(file, line)) {
-            int i = 0;
-            string question, answer;
-
-            // question until @
-            while (line[i] != '@') {
-                question += line[i];
-                ++i;
-            }
-            ++i;
-
-            // answer until end
-            while (i < line.length()) {
-                answer += line[i];
-                ++i;
-            }
-
-            this->solutions.insert(pair<string,string>(question,answer));
-        }
-        file.close();
-    }
-}
-
-template<class Problem, class Solution>
-void FileCacheManager<Problem, Solution>::save() {
-    ofstream file(CACHE_FILE_NAME);
-
-    if (file.is_open()) {
-        for (auto &qa : this->solutions) {
-            file << qa.first << "@" << qa.second << endl;
-        }
-        file.close();
-    } else {
-        throw "unable to open file";
-    }
-}
-
-template<class Problem, class Solution>
-FileCacheManager<Problem, Solution>::FileCacheManager() {
-    this->load();
-}
-
-template<class Problem, class Solution>
-FileCacheManager<Problem, Solution>::~FileCacheManager() {
-    this->save();
-}
 #endif //SOLIDPROJECT_EX2_FILECACHEMANAGER_H
